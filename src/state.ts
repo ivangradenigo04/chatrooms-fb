97192f2aa3f3f38832a9b841dc161f8275147806
Ivan Gradenigo <ivigradenigo@gmail.com>
2024-01-01 18:09:01 -0300

    update state.ts

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,7 @@
 import { rtdb, db } from "./rtdb";
 import { map } from "lodash";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
 
 const state = {
   data: {
